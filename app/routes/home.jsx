629c8765f03d54aaf83d738f9cf5e7f8b9e3d6e0
Ayman <aymantoumi99@gmail.com>
2023-03-12 18:58:54 +0100

    Hello mom

---
+++ app/routes/home.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+export default function Home() {
+  return (
+    <>
+    <Link to='/' > Index </Link>
+    <h3>I'm Home </h3>
+    </>
+  );
+}
---
+++ app/routes/index.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+export default function Index() {
+  return (
+    <>
+    <Link to='/home' > Home </Link>
+    <h3>I'm index </h3>
+    </>
+  );
+}
