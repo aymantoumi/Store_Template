5e76f2a643662d28146ebb5b317a41fb535a6c27
Ayman <aymantoumi99@gmail.com>
2023-03-13 13:17:54 +0100

    nothing

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 export default function Index() {
   return (
     <>
-    <Link to='/home' > Home </Link>
-    <h3>I'm your mom </h3>
+      <Link to='/home' > Home </Link>
+      <h3>I'm your mom </h3>
+      <p>
+        fuck you
+      </p>
     </>
   );
 }
